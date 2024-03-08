ac384f983a2f97cf191910fa567e642186ec1eeb
moirachen1019 <peggy10191019@gmail.com>
2024-03-08 07:31:20 +0000

    version 1 script

--- Xecute.sh
+++ Xecute.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
-# directories=("Main" "Memory" "Explorer" "Collection" "LossCollection")
-directories=("LossCollection")
+directories=("Collection" "Explorer" "Memory" "MemoryRelation")
 # Loop through the directories
 for dir in "${directories[@]}"; do
   # Check if the specified directory exists
@@ -25,53 +24,3 @@ for dir in "${directories[@]}"; do
   done
   cd ..
 done
-
-# array1=("ed_appresourceusagemonitor" "ed_arpcache" "ed_baseservice" "ed_chromebookmarks" "ed_chromecache" "ed_chromedownload" "ed_chromehistory" "ed_chromekeywordsearch" "ed_chromelogin" "ed_dnsinfo")
-# array2=("ed_edgebookmarks" "ed_edgecache" "ed_edgecookies" "ed_edgehistory" "ed_edgelogin" "ed_eventapplication" "ed_eventsecurity" "ed_eventsystem" "ed_firefoxbookmarks" "ed_firefoxcache")
-
-# for index in ${array2[@]}; do
-#     echo $index
-#     curl -X DELETE "http://192.168.200.190:9200/$index?pretty"
-# done
-
-# ./Collection/AppResourceUsageMonitor.sh  
-# ./Collection/ChromeCache.sh          
-# ./Collection/ChromeLogin.sh    
-# ./Collection/EdgeCookies.sh       
-# ./Collection/EventSecurity.sh     
-# ./Collection/ARPCache.sh                 
-# ./Collection/ChromeDownload.sh       
-# ./Collection/DNSInfo.sh        
-# ./Collection/EdgeHistory.sh       
-# ./Collection/EventSystem.sh       
-# ./Collection/BaseService.sh              
-# ./Collection/ChromeHistory.sh        
-# ./Collection/EdgeBookmarks.sh  
-# ./Collection/EdgeLogin.sh         
-# ./Collection/FirefoxBookmarks.sh
-# ./Collection/ChromeBookmarks.sh          
-# ./Collection/ChromeKeywordSearch.sh  
-# ./Collection/EdgeCache.sh      
-# ./Collection/EventApplication.sh  
-# ./Collection/FirefoxCache.sh
-# ./Collection/FirefoxCookies.sh
-# ./Collection/FirefoxHistory.sh
-# ./Collection/IEHistory.sh
-# ./Collection/InstalledSoftware.sh
-# ./Collection/JumpList.sh
-# ./Collection/MUICache.sh
-# ./Collection/Network.sh
-# ./Collection/NetworkDataUsageMonitor.sh
-# ./Collection/NetworkResources.sh
-# ./Collection/OpenedFiles.sh
-# ./Collection/Prefetch.sh
-# ./Collection/Process.sh
-# ./Collection/Service.sh
-# ./Collection/Shortcuts.sh
-# ./Collection/TaskSchedule.sh
-# ./Collection/StartRun.sh
-# ./Collection/USBDevices.sh
-# ./Collection/UserAssist.sh
-# ./Collection/UserProfiles.sh
-# ./Collection/WindowsActivity.sh
-# ./Collection/SqliteSequence.sh
